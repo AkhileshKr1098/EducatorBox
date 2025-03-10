559a612a13d345e978c776e15546679378f55250
Akhilesh <tech.kr.akhi@gmail.com>
2025-03-10 10:31:40 +0530

    certificated good

--- src/app/institute/certificate/certificate.component.ts
+++ src/app/institute/certificate/certificate.component.ts
@@ -21,7 +21,7 @@ export class CertificateComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   cer_count: string = "0"
-  imgUrl: string = 'https://educatorbox.com/assets/certificate/';
+  imgUrl: string = 'https://cms.sankalpcomputer.com/assets/certificate/';
   inst_id: any
   login: any
   login_deatils: any
